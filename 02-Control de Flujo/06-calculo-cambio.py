8d81656edea5869f539e63a03a33b84a4aa9bbda
msianodev <matias.siano@outlook.com>
2025-01-20 12:33:43 -0300

    [02 Control del Flujo] - 06 calculo de cambio

--- 02-Control de Flujo/06-calculo-cambio.py
+++ 02-Control de Flujo/06-calculo-cambio.py
@@ -12,3 +12,24 @@
 #
 # 3.Entrada: Total: 75, Pago: 100.
 # Salida:" El cliente ha pagado demás. El cambio es 25.0 dólares”.
+
+print("Calculo de Cambio")
+
+total_cuenta = float(input("Ingrese el total de la cuenta: "))
+
+pago_cliente = float(input("Ingrese el monto pagado por el cliente: "))
+
+if total_cuenta <= 0 or pago_cliente <= 0:
+    print("Ingrese un total y un pago validos.")
+    exit()
+
+if pago_cliente == total_cuenta:
+    print("El cliente ha pagado el monto exacto. No se requiere cambio.")
+
+elif pago_cliente < total_cuenta:
+    print(f"El cliente ha pagado menos. Faltan cobrar $ {
+          (total_cuenta - pago_cliente):.2f} dólares.")
+
+else:
+    print(f"El cliente ha pagado demás. El cambio es de $ {
+          (pago_cliente - total_cuenta):.2f} dólares.")
